3ddb5847b5cfd1a3b5bb50a6d90f58c401128d83
Siddharth Saha <sisahawork@gmail.com>
2023-08-27 16:32:15 -0400

    Update pkg_name.launch.py

--- pkg_name_can/launch/pkg_name.launch.py
+++ pkg_name_can/launch/pkg_name.launch.py
@@ -1,7 +1,6 @@
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
-from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
 import os
